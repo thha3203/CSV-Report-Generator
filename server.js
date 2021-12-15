b46880c520b4d9903883eb9ff3434682d5146ad3
Thanh Ha <thanh3203@gmail.com>
2021-12-15 10:25:07 -0800

    Implemented response to send back a csv file for user to download

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const jsonToCSV = require('./helpers/jsonToCSV.js');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('client'));
+app.use('/', express.static('client'));
+app.use('/convert', express.static('csvTable'));
 
 
 app.post('/convert', (request, response) => {
   var file = jsonToCSV(request.body.jsonFile);
+  response.set('Content-Type', 'text/csv');
   response.send(file);
 });
 
